d543b8b7c8b116a87ae83541bfa7a8c677840962
Michal Gawik <gawlik.zator@o2.pl>
2024-09-25 13:03:42 +0200

    add job buttons and style

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
 	return (
 		<section className='jobs-center'>
 			<JobInfo data={data} currentItem={currentItem} />
-			<BtnContainer />
+			<BtnContainer data={data} currentItem={currentItem} />
 		</section>
 	)
 }
--- src/components/BtnContainer.tsx
+++ src/components/BtnContainer.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { type TabType } from '../data'
 
-const BtnContainer = () => {
-	return <div></div>
+type BtnContainerProps = {
+	data: TabType[]
+	currentItem: number
+}
+
+const BtnContainer = ({ data, currentItem }: BtnContainerProps) => {
+	return (
+		<div className='btn-container'>
+			{data.map(item => {
+				return (
+					<button key={item.id} className='job-btn' type='button'>
+						{item.company}
+					</button>
+				)
+			})}
+		</div>
+	)
 }
 
 export default BtnContainer
